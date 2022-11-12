e995b1cd94f3f209f6ab2e93a40d3c06d17e1c49
Kartik Chawla <kchawla1995@gmail.com>
2022-11-11 19:10:58 -0500

    Added a basic spotify webhook

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/spotify", (req, res) => {
+    console.log("in spotify", req.query);
+    return res.json({ message: "Ok report" })
+});
+
+console.log(`App listening on port: ${port}`);
+app.listen(port);
