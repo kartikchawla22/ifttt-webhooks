bd984b268c67e4a809f82c411595704c59f43158
bharath2059 <mamidibharathreddy2059@gmail.com>
2022-11-11 21:57:11 -0500

    added webhook for github

--- index.js
+++ index.js
@@ -4,16 +4,20 @@ const app = express();
 const port = 3000;
 
 app.get("/spotify", (req, res) => {
-    console.log("in spotify", req.query);
-    return res.json({ message: "Ok report" })
+  console.log("in spotify", req.query);
+  return res.json({ message: "Ok report" });
 });
 
 app.get("/calender", (req, res) => {
+  console.log("in calender", req.query);
 
-    console.log("in calender", req.query);
+  return res.json({ message: "Ok report" });
+});
 
-    return res.json({ message: "Ok report" })
+app.get("/github", (req, res) => {
+  console.log("in github", req.query);
 
+  return res.json({ message: "Ok report" });
 });
 console.log(`App listening on port: ${port}`);
 app.listen(port);
